2f89405c47728170a0f6d37c326aa1f86a9b1a3e
Rodolfo Ramírez Cedillo <rodormz@tec.mx>
2019-11-08 17:27:22 -0600

    Fixed popup bug on Empatia modal

--- assets/js/dignidad.js
+++ assets/js/dignidad.js
@@ -1,23 +1,13 @@
 var modalEmpatia = document.getElementById("myModal")
-var modalFlorecimiento = document.getElementById("modal-florecimiento")
-var btn4 = document.getElementById("myBtn4")
-var closeFlorecimiento = document.getElementsByClassName("closeModal")[2]
-var closeEmpatia = document.getElementsByClassName("closeModal")[4]
+var closeEmpatia = document.getElementsByClassName("closeModal")[2]
 
 // When the user clicks on <span> (x), close the modal
 closeEmpatia.onclick = function() {
     modalEmpatia.style.display = "none";
 }
-closeFlorecimiento.onclick = function() {
-  modalFlorecimiento.style.display = "none";
-};
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modalFlorecimiento) {
     modalFlorecimiento.style.display = "none";
   }
-};
-
-btn4.onclick = function() {
-  modalFlorecimiento.style.display = "block";
 };
