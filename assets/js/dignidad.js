f66095ee7ac452e3a6d5df2a776584acbd35f924
Rodolfo <rodormz98@gmail.com>
2019-12-19 15:34:59 -0600

    incomplete parts were hidden

--- assets/js/dignidad.js
+++ assets/js/dignidad.js
@@ -1,13 +1,41 @@
-var modalEmpatia = document.getElementById("myModal")
-var closeEmpatia = document.getElementsByClassName("closeModal")[2]
+let modalEmpatia = document.querySelector("#myModal")
+let modalEstructura = document.querySelector('#myModal-estructura')
+let modalRedParticipacion = document.querySelector('#myModal-participacion')
+let closeModal = document.querySelector(".closeModal")
+let myBtnEstructura = document.querySelector('#myBtn6')
+let myBtnEmpatia = document.querySelector('#myBtn')
+let myBtnRedParticipacion = document.querySelector('#myBtn7')
+
 
 // When the user clicks on <span> (x), close the modal
-closeEmpatia.onclick = function() {
+closeModal.onclick = () => {
     modalEmpatia.style.display = "none";
+    modalEstructura.style.display ="none";
+    modalRedParticipacion.style.display = "none";
 }
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modalFlorecimiento) {
-    modalFlorecimiento.style.display = "none";
+window.onclick = (event) => {
+  if (event.target === modalEmpatia ) {
+    modalEmpatia.style.display = "none";
+  }
+  
+  if (event.target === modalEstructura){
+    modalEstructura.style.display = "none";
+  }
+  
+   if(event.target === modalRedParticipacion){
+    modalRedParticipacion.style.display = "none";
   }
 };
+
+myBtnEstructura.onclick = () => {
+  modalEstructura.style.display = "block";
+};
+
+myBtnEmpatia.onclick = () => {
+  modalEmpatia.style.display = "block";
+}
+
+myBtnRedParticipacion.onclick = () => {
+  modalRedParticipacion.style.display = "block";
+}
